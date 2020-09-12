feb96921aa8323dd9fc89216d01af50216d01090
GT Fari <guideg6@gmail.com>
2020-09-12 12:34:08 +0200

    google auth working & saving to db

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
 const connectDB = require('./config/db')
+const { Session } = require('inspector')
 
 // Load config
 dotenv.config({path: './config/config.env'})
+
+// Passport config
+require('./config/passport')(passport)
 
 connectDB()
 
@@ -21,12 +27,27 @@ if (process.env.NODE_ENV === 'development'){
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', '.hbs')
 
+// Sessions
+app.use(
+    session({
+      secret: 'keyboard cat',
+      resave: false,
+      saveUninitialized: false,
+    //   store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    })
+  )
+
+// Passport Middlewear
+app.use(passport.initialize())
+app.use(passport.session())
+
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Routes
 app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
 
 const PORT = process.env.PORT || 5000
 
